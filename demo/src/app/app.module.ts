ed89f216551e82ed6f6c6c8200e9c5440908a9c9
sushaj <sushajxui@gmail.com>
2020-01-13 18:58:39 +0530

    edited Signed-off-by: sushaj <sushajxui@gmail.com>

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule,
