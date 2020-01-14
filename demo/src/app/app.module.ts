29a69e02c1a96ea3069ca4bceae5d04cb3d3a476
sushajrs <59275609+sushajrs@users.noreply.github.com>
2020-01-14 15:37:46 +0530

    Update app.module.ts

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule,
     FormsModule,
     NgbModule  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
