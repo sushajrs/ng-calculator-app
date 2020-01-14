60dbc78401c0aafc15dd4b26ec6ce5a802afbc0b
sushajrs <59275609+sushajrs@users.noreply.github.com>
2020-01-14 12:39:51 +0530

    Update app.module.ts

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-  ],
+    FormsModule,
+    NgbModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
